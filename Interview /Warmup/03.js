00e484241c53e7a21a5f9fabd199cd884870151b
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-01 16:03:12 -0700

    fixed the stupid bug

--- Interview /Warmup/03.js
+++ Interview /Warmup/03.js
@@ -1,23 +1,23 @@
-function jumpingOnClouds(n) {
-  const c = n.map( x => Number(x))
-  const path = []
-  let i = -1
-  while(i < c.length-1){
-    if (c[i+2] === 0 || i+2 >= c.length-1){
-      if(i+2 <= c.length-1){
-        path.push(i+2)
-      }
+function jumpingOnClouds(c) {
+  const path = [0]
+  let jumps = 0
+  let i = 0
+  while(i < c.length){
+    if (c[i+2] === 0){
+      path.push(i+2)
+      jumps++
       i = i+2
-    }else{
-      path.push(i+1)
+    }else {
+      if (c[i+1] === 0){
+        path.push(i+1)
+        jumps++
+      }
       i++
     }
   }
   console.log(path)
-  return path.length
+  return jumps
 
 }
-const str = '0 1 0 1 0 1 0 1 0 1 0 0 0 1 0 0 0 1 0 0 0 0 0 0 1 0 1 0 1 0 0 0 0 0 1 0 0 1 0 1 0 0 1 0 1 0 1 0 0 0 0 1 0 0 1 0 0 1 0 1 0 0 1 0 1 0 0 1 0 1 0 1 0 1 0 0 0 1 0 1 0 0 0 1 0 1 0 0 0 1 0 1 0 1 0 0 0 0 1 0'
 
 console.log(jumpingOnClouds([0,0,0,1,0,0]))
-console.log(jumpingOnClouds(str.split(" ")))
