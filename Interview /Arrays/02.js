a27e79b85e5d56373d8d1393930f7b7e9e949963
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-04 15:36:26 -0700

    02 arrays done was pretty easy

---
+++ Interview /Arrays/02.js
@@ -0,0 +1,14 @@
+
+function rotLeft(a, d) {
+  let result = []
+  for(let i = d; i < a.length; i++){
+    result.push(a[i])
+  }
+  for(let i = 0; i < d; i++){
+    result.push(a[i])
+  }
+  return result
+
+}
+
+console.log(rotLeft([1,2,3,4,5], 4))
