56941cced305c801b2563bc0a4d340715e2c6dba
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-04 15:32:47 -0700

    :3

--- Interview /Arrays/01.js
+++ Interview /Arrays/01.js
@@ -2,16 +2,9 @@ function hourglassSum(arr) {
   const results = []
   for(let i=0 ; i<arr.length-2; i++){
     for(let j=0 ; j<arr.length-2; j++){
-      console.log(arr[i][j],arr[i][j+1],arr[i][j+2])
-      console.log(arr[i+1][j+1])
-      console.log(arr[i+2][j],arr[i,2][j+1],arr[i+2][j+1])
-      console.log(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
-      console.log('--------------------------------')
-      
       results.push(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
     }
   }
-  console.log(results)
   return Math.max(...results)
 }
 
